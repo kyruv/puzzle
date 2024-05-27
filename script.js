cc295642090269b00aea790cb2304399e57f2750
kyruv <kylerruvane@gmail.com>
2024-05-27 17:56:08 -0600

    2 puzzles

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // The non-obfuscated script
 (function () {
     const encodedAnswers = {
-        1: 'ZEdWdGNHOXlZWEo1SUdGdWMzZGxjZz09' // base64 x2 encoding of 'temporary answer'
+        1: 'TVRrMk56QXpNVGc9', // base64 x2 encoding of '19670318'
+        2: 'Wm1Wc1pHbHc=' // 'feldip'
     };
 
     function checkAnswer(puzzleNumber) {
@@ -24,10 +25,23 @@
             puzzleContainer.innerHTML = `
                 <h2>Second Puzzle</h2>
                 <p>Congratulations! You unlocked the second puzzle. Solve this one to proceed.</p>
-                <!-- Add content for the second puzzle here -->
+                <h4>Expert: Ryan</h4>
+                <p>The hills of AKS.</p>
                 <input type="text" id="answer-2" placeholder="Enter your answer here">
                 <button onclick="checkAnswer(2)">Submit</button>
                 <p id="message-2" class="message"></p>
+            `;
+            puzzleContainer.classList.remove('hidden');
+        }
+        if (puzzleNumber === 3) {
+            puzzleContainer.innerHTML = `
+                <h2>Third Puzzle</h2>
+                <p>Congratulations! You unlocked the third puzzle. Solve this one to proceed.</p>
+                <h4>Expert: Adam</h4>
+                <p>TODO</p>
+                <input type="text" id="answer-3" placeholder="Enter your answer here">
+                <button onclick="checkAnswer(3)">Submit</button>
+                <p id="message-3" class="message"></p>
             `;
             puzzleContainer.classList.remove('hidden');
         }
